c5e8f72992a9d63c5b611dfba1a0add2a479fdfa
michaelkincheloe <michael.kincheloe@wustl.edu>
2019-08-29 13:35:36 -0500

    hi

---
+++ src/studio1/Average.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import support.cse131.ArgsProcessor;
+
+public class Average
+{
+    public static void main(String[] args)
+    {
+        ArgsProcessor ap = new ArgsProcessor(args);
+        int n1 = ap.nextInt("What's your first int?");
+        int n2 = ap.nextInt("What's your next int");
+
+        double avg = (double)(n1 + n2)/2;
+
+        System.out.println("Your average is: " + avg);
+    }
+}
+
